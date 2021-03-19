8bfa00cbf1cb1fbc146b08bc9f9461699b6ed114
Geoff <geosmile22@gmail.com>
2021-03-19 14:26:16 -0600

    add MonthDayAtTime method

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.year = exports.add = exports.timeago = exports.monthDayYearAtTime = exports.monthDayYear = undefined;
 
-var _moment = require('moment');
+var _moment = require("moment");
 
 var _moment2 = _interopRequireDefault(_moment);
 
-require('moment-timezone');
+require("moment-timezone");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
 
-var monthDayYear = exports.monthDayYear = function monthDayYear(timestamp, timezone) {
-  return !timezone ? (0, _moment2.default)(timestamp).format('MMMM Do, YYYY') : (0, _moment2.default)(timestamp).tz(timezone).format('MMMM Do, YYYY');
-};
+var monthDayYear = (exports.monthDayYear = function monthDayYear(timestamp, timezone) {
+  return !timezone ? (0, _moment2.default)(timestamp).format("MMMM Do, YYYY") : (0, _moment2.default)(timestamp).tz(timezone).format("MMMM Do, YYYY");
+});
 
-var monthDayYearAtTime = exports.monthDayYearAtTime = function monthDayYearAtTime(timestamp, timezone) {
-  return !timezone ? (0, _moment2.default)(timestamp).format('MMMM Do, YYYY [at] hh:mm a') : (0, _moment2.default)(timestamp).tz(timezone).format('MMMM Do, YYYY [at] hh:mm a');
-};
+var monthDayYearAtTime = (exports.monthDayYearAtTime = function monthDayYearAtTime(timestamp, timezone) {
+  return !timezone
+    ? (0, _moment2.default)(timestamp).format("MMMM Do, YYYY [at] hh:mm a")
+    : (0, _moment2.default)(timestamp).tz(timezone).format("MMMM Do, YYYY [at] hh:mm a");
+});
 
-var timeago = exports.timeago = function timeago(timestamp, timezone) {
+var monthDayAtTime = (exports.monthDayAtTime = function monthDayAtTime(timestamp, timezone) {
+  return !timezone
+    ? (0, _moment2.default)(timestamp).format("MMMM Do,[at] hh:mm a")
+    : (0, _moment2.default)(timestamp).tz(timezone).format("MMMM Do,[at] hh:mm a");
+});
+
+var timeago = (exports.timeago = function timeago(timestamp, timezone) {
   return !timezone ? (0, _moment2.default)(timestamp).fromNow() : (0, _moment2.default)(timestamp).tz(timezone).fromNow();
-};
+});
 
-var add = exports.add = function add(timestamp, amount, range, timezone) {
+var add = (exports.add = function add(timestamp, amount, range, timezone) {
   return !timezone ? (0, _moment2.default)(timestamp).add(amount, range).format() : (0, _moment2.default)(timestamp).tz(timezone).add(amount, range).format();
-};
+});
 
-var year = exports.year = function year(timestamp, timezone) {
-  return !timezone ? (0, _moment2.default)(timestamp).format('YYYY') : (0, _moment2.default)(timestamp).tz(timezone).format('YYYY');
-};
+var year = (exports.year = function year(timestamp, timezone) {
+  return !timezone ? (0, _moment2.default)(timestamp).format("YYYY") : (0, _moment2.default)(timestamp).tz(timezone).format("YYYY");
+});
